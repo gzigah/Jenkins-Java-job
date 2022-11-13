8b244357837a694a398784b96c9e334928742967
Godwin Zigah <gzigah@yahoo.com>
2022-11-13 10:03:20 -0700

    Reverted back to original
    Reverted back to original

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,6 @@
  Public Class HelloWorld {
 	 Public static void main(String[] args) {
 		 System.out.printIn("Hello, World");
-		 System.out.printIn("Hello, Jenkins");
-		 System.out.printIn("Hello, Jenkins2");
 	 }
  }
 
